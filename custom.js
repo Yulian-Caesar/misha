8c62c61674615415878b7fb98db5b79b2cf22a1d
Yulian Khai <yulik19971997@gmail.com>
2022-12-16 22:35:09 +0200

    update

--- custom.js
+++ custom.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+    $("#menu, #sidebar__menu").on("click", ".menu__link, .sidebar__menu-link", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top - 80;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
 $(".toggle-popup").on('click', function() {
     $("#popup").toggleClass('open');
 }); 
