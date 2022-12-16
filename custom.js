d2a94e8216f2265608a2b993f25d9d26a53e5329
Yulian Khai <yulik19971997@gmail.com>
2022-12-16 21:31:06 +0200

    remove anchor

--- custom.js
+++ custom.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    $("#menu, #sidebar__menu").on("click", ".menu__link, .sidebar__menu-link", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top - 80;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-});
+// $(document).ready(function(){
+//     $("#menu, #sidebar__menu").on("click", ".menu__link, .sidebar__menu-link", function (event) {
+//         event.preventDefault();
+//         var id  = $(this).attr('href'),
+//             top = $(id).offset().top - 80;
+//         $('body,html').animate({scrollTop: top}, 1500);
+//     });
+// });
 
 
 $("header").removeClass("fixed");
