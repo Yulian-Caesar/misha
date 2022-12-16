2184acd0eeeb912172a6d936d0ca001a1b7dd5bf
Yulian Khai <yulik19971997@gmail.com>
2022-12-16 21:58:54 +0200

    check performance

--- custom.js
+++ custom.js
@@ -1,14 +1,3 @@
-
-$("header").removeClass("fixed");
-var introHeight = $(".home").height()
-$(window).scroll(function(){
-    if ($(this).scrollTop() > introHeight) {
-    $("header").addClass("fixed");
-    } else {
-    $("header").removeClass("fixed");
-    };
-});
-
 $(".toggle-popup").on('click', function() {
     $("#popup").toggleClass('open');
 }); 
